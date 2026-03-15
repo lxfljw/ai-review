6dca7806041f061c7c52f926e9a1ac129406d7f0
卢晓锋 <lxfljw@gmail.com>
2026-03-15 12:53:15 +0800

    fix: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+const a = 4;
+a.t = 55;
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
