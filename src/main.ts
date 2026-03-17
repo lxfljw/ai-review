64648ce35cf13ae4f689f3886ec7cfb82aaed0b8
卢晓锋 <lxfljw@gmail.com>
2026-03-17 15:57:30 +0800

    feat✨: 22

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 const a = 4;
-a.t = "fefnnfdewhf rr";
+a.t = "fef热nnfdewhf 然而rr";
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
